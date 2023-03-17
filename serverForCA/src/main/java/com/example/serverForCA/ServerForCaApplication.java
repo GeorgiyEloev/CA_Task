51fc3f2fe82fbd853bcf4c6ced9f6e3178e9f2a5
GeorgiyEloev <geloev.exceedteam@gmail.com>
2023-03-17 11:09:29 +0300

    basis of the server

---
+++ serverForCA/src/main/java/com/example/serverForCA/ServerForCaApplication.java
@@ -0,0 +1,13 @@
+package com.example.serverForCA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerForCaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerForCaApplication.class, args);
+	}
+
+}
---
+++ serverForCA/src/test/java/com/example/serverForCA/ServerForCaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverForCA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerForCaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
